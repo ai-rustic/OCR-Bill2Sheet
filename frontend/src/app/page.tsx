a9cf03b9c38a532972cbd47225750a658889596c
truongducdung7199 <truongducdung.98@gmail.com>
2025-09-08 15:31:20 +0700

    Fix port bug

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
         formData.append('bill_images', file);
       });
 
-      const response = await fetch('http://10.1.4.189:8888/api/ocr-bill', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/ocr-bill`, {
         method: 'POST',
         body: formData,
       });
